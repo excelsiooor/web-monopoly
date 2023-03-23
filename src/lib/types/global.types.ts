095d7e890f1dd916b19754f6e86e3fdf37222fdb
excelsiooor <homenich101@gmail.com>
2023-03-23 11:45:51 +0200

    provide router

--- src/lib/types/global.types.ts
+++ src/lib/types/global.types.ts
@@ -1,3 +1,12 @@
+import React from 'react';
+
 export interface IMap {
   [key: string | number]: any;
+}
+
+
+export interface RouteType {
+  path: string
+  element: React.ReactNode
+  exact: boolean
 }
