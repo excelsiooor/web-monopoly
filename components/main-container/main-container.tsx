b44c33f12fdb74bb25bef640433647fbe23e2a66
excelsiooor <homenich101@gmail.com>
2023-03-28 18:53:47 +0300

    rename

--- components/main-container/main-container.tsx
+++ components/main-container/main-container.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react'
 import cls from 'classnames'
 import { IDefaultComponentProps } from '../../lib/types/component.types'
-import styles from './sign-up-container.module.scss'
+import styles from './main-container.module.scss'
 
 interface IComponentProps extends IDefaultComponentProps {
   children: React.ReactNode
 }
 
-export const SugnUpContainer: FC<IComponentProps> = ({ style, className, children }) => (
+export const MainContainer: FC<IComponentProps> = ({ style, className, children }) => (
   <main style={style} className={cls(className, styles.wrapper)}>
     {children}
   </main>
